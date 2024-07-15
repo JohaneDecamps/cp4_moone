1c46ff126730c8fb1ae6a45130a1afd2b6f83047
JohaneDecamps <johane.decamps@gmail.com>
2024-07-16 00:04:11 +0200

    feat: HomePage and NavBar

---
+++ client/src/components/navbar/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import avatar from "../../assets/logos/avatar-home.png";
+
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <div className="link-nav">
+        <Link to="/"> SAVOIR FAIRE  </Link>
+        <Link to="/collection"> LA COLLECTION </Link>
+        
+        <Link to="/connexion">
+          <img src={avatar} alt="avatar" className="avatar" />{" "}
+        </Link>
+        </div>
+      </ul>
+    </nav>
+  );
+}
---
+++ client/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,12 @@
+import "./HomePage.css"
+import img from "../../assets/images/img-homepage.jpeg";
+
+export default function HomePage() {
+  return (
+
+   
+
+      <img src={img} className="photo-homage" alt="photographie" />
+
+  );
+}
