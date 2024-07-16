7c67fba213909f02fe5f47add6f2142acc5f9ca1
JohaneDecamps <johane.decamps@gmail.com>
2024-07-16 20:24:26 +0200

    feat: category page and article card

---
+++ client/src/components/articlecard/ArticleCard.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import "./ArticleCard.css";
+
+export default function ArticleCard({ article }) {
+  const { reference, image } = article;
+
+  return (
+    <section className="container-card">
+   
+      <img src={image} alt="" className="image-card" />
+      <div className="explication-card"> 
+      <h1 className="reference-card"> {reference}  </h1>
+      {/* <h2 className="description-card"> {description} </h2> */}
+      </div>
+    
+    </section>
+    
+  );
+}
+
+ArticleCard.propTypes = {
+  article: PropTypes.string.isRequired,
+  reference: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
---
+++ client/src/pages/categoryPage/CategoryPage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ArticleCard from "../../components/articlecard/ArticleCard";
+
+import imgCollection from "../../assets/images/img-collectionpage.jpeg";
+import "./CategoryPage.css"
+
+export default function CategoryPage() {
+
+const [result, setResult] = useState()
+const categoryName = useParams();
+console.info(result)
+
+  useEffect(()=>{
+      const fetchCategory = async () => {
+          const response = await fetch(
+              `http://localhost:3310/api/categories/${categoryName.name}`
+          );
+          const data = await response.json();
+          setResult(data)
+      };
+      fetchCategory()
+  }, [categoryName])
+
+    return (
+        <> 
+        <img
+        src={imgCollection}
+        className="photo-category"
+        alt="photographie"
+      />
+
+<h1 className="title-categoryPage"> {categoryName.name} </h1>
+        <div className="container-articlecard"> 
+            
+
+    {result?.map((b) => (
+        <ArticleCard key={b} article={b}/>
+    ))}
+  </div>
+    </>
+    )
+}
