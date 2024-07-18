7b64a6e38e6a68f3801031cd4cc89fc301414dbe
JohaneDecamps <johane.decamps@gmail.com>
2024-07-18 09:18:33 +0200

    feat: form delete article

--- client/src/components/articlesforms/ArticlesAdd.jsx
+++ client/src/components/articlesforms/ArticlesAdd.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-import "./ArticlesAdd.css"
+import "./ArticlesAdd.css";
 
 export default function ArticlesAdd() {
   const [category, setCategory] = useState();
@@ -12,7 +12,7 @@ export default function ArticlesAdd() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-
+  console.info("categorie:", category);
   const onSubmit = async (dataArticles) => {
     try {
       await axios.post("http://localhost:3310/api/articles", dataArticles);
@@ -32,72 +32,84 @@ export default function ArticlesAdd() {
       console.error(err);
     }
   }, []);
-  console.info("categorie:", category);
 
   return (
     <section className="form-add-container">
       <h1 className="title-form"> Ajouter un article </h1>
-      <form className="form-add-article"onSubmit={handleSubmit(onSubmit)}>
-        <div className="input-add-article"> 
-        <label htmlFor="reference"> Reference </label>
-        <input
-          type="text"
-          name="reference"
-          className="input-reference-add"
-          {...register("reference", {
-            required: "ce champs est requis",
-          })}
-        />
-        {errors.reference && <span> {errors.reference.message} </span>}
-</div>
-<div className="input-add-article"> 
-        <label htmlFor="description" className="input-description-add"> Description </label>
-        <input
-          type="text"
-          name="description"
-          className="input-description-article"
-          {...register("description", {
-            required: "ce champs est requis",
-            minLength: {
-              value: 2,
-              message: "La description doit contenir au minimum 2 caractère",
-            },
-          })}
-        />
-        {errors.description && <span> {errors.description.message} </span>}
-</div>
-<div className="input-add-article"> 
-        <label htmlFor="categories"> Categorie </label>
-        <select
-          name="category"
-          className="input-category-article"
-          {...register("category_id", {
-            required: "Veuillez selectionner une categorie",
-          })}
-        >
-          <option value="" className="input-category-article"> Selectionnez une categorie </option>
-          {category?.map((c) => (
-            <option className="input-category-article" key={c.id} value={c.id}>
-              {c.name}
-            </option>
-          ))}
-        </select>
+      <form className="form-add-article" onSubmit={handleSubmit(onSubmit)}>
+        <div className="input-add-article">
+          <label htmlFor="reference"> Reference </label>
+          <input
+            type="text"
+            name="reference"
+            className="input-reference-add"
+            {...register("reference", {
+              required: "ce champs est requis",
+            })}
+          />
+          {errors.reference && <span> {errors.reference.message} </span>}
         </div>
-        <div className="input-add-article"> 
-        <label htmlFor="image"> Image </label> 
-        <input 
-        type="text"
-        name="image"
-        placeholder="Veuillez entrer une url"
-        className="input-image-article"
-        {...register("image", {
-            required: "ce champs est requis"
-        })}/>
-        {errors.image && <span> {errors.image.message} </span> }
-     </div>
-        <button className="button-add-article" type="submit"> Ajouter un article </button>
+        <div className="input-add-article">
+          <label htmlFor="description" className="input-description-add">
+            {" "}
+            Description{" "}
+          </label>
+          <input
+            type="text"
+            name="description"
+            className="input-description-article"
+            {...register("description", {
+              required: "ce champs est requis",
+              minLength: {
+                value: 2,
+                message: "La description doit contenir au minimum 2 caractère",
+              },
+            })}
+          />
+          {errors.description && <span> {errors.description.message} </span>}
+        </div>
+        <div className="input-add-article">
+          <label htmlFor="categories"> Categorie </label>
+          <select
+            name="category"
+            className="input-category-article"
+            {...register("category_id", {
+              required: "Veuillez selectionner une categorie",
+            })}
+          >
+            <option value="" className="input-category-article">
+              {" "}
+              Selectionnez une categorie{" "}
+            </option>
+            {category?.map((c) => (
+              <option
+                className="input-category-article"
+                key={c.id}
+                value={c.id}
+              >
+                {c.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="input-add-article">
+          <label htmlFor="image"> Image </label>
+          <input
+            type="text"
+            name="image"
+            placeholder="Veuillez entrer une url"
+            className="input-image-article"
+            {...register("image", {
+              required: "ce champs est requis",
+            })}
+          />
+          {errors.image && <span> {errors.image.message} </span>}
+        </div>
+        <button className="button-add-article" type="submit">
+          {" "}
+          Ajouter un article{" "}
+        </button>
       </form>
-
     </section>
   );
 }
--- client/src/pages/adminpage/AdminPage.jsx
+++ client/src/pages/adminpage/AdminPage.jsx
@@ -1,11 +1,13 @@
 import ArticleAdd from "../../components/articlesforms/ArticlesAdd"
 import CategoryAdd from "../../components/categoryform/CategoryAdd"
+import ArticleDelete from "../../components/articlesforms/ArticleDelete"
 
 export default function AdminPage () {
     return (
         <> 
     <ArticleAdd/>
     <CategoryAdd/>
+    <ArticleDelete/>
     </>
     )
 }
